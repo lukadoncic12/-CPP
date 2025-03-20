0d99441099b70cf093e8d6a0219bffb7782918fa
lukadoncic12 <149558606+lukadoncic12@users.noreply.github.com>
2025-03-20 13:55:45 +0800

    Add files via upload

--- 求二叉树最大深度.cpp
+++ 求二叉树最大深度.cpp
@@ -9,22 +9,26 @@ struct BTree{
 
     BTree* Lchild;
     BTree* Rchild;
+    BTree(int x):value(x),Lchild(nullptr),Rchild(nullptr){}
 };
 
 //判断最大深度
 int max_depth(BTree* root){
 
-    if(root == NULL){ //和null比较的必须是指针，&root不是指针， BTree* root才是指针
+    if(root == nullptr){ //和nullptr比较的必须是指针，&root不是指针， BTree* root才是指针
         return 0;
     }
 
-    return max(max_depth(root->Lchild),max_depth(root->Rchild)); //指针不能点（.） 要用箭头（->）
+
+    return 1 + max(max_depth(root->Lchild),max_depth(root->Rchild)); //指针不能点（.） 要用箭头（->）
 }
 
 
 int main(){
 
-    BTree* tree_example;
-    max_depth(tree_example);
+    BTree* root = new BTree(1);
+    root->Lchild = new BTree(2);
+    root->Rchild = new BTree(3);
+    cout << max_depth(root);
     return 0;
 }
